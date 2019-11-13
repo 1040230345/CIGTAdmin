4d71d4ae73cdcf7e01b58c6bf829f85e6ae35b36
Jiesong.Zhang <javajies@163.com>
2019-11-13 14:46:22 +0800

    first commit

---
+++ src/main/java/com/cigt/CigtadminApplication.java
@@ -0,0 +1,13 @@
+package com.cigt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CigtadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CigtadminApplication.class, args);
+    }
+
+}
